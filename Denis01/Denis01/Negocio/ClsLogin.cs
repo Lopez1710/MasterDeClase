dcc77066ade9abbb763d8cfbe91e592bb184f059
denis lopez <lo0706032020@gmail.com>
2021-02-09 16:32:59 -0600

    Login

---
+++ Denis01/Denis01/Negocio/ClsLogin.cs
@@ -0,0 +1,24 @@
+﻿using Denis01.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denis01.Negocio
+{
+    class ClsLogin
+    {
+        public Boolean logeo(Login log)
+        {
+            if(log.User.Equals("Denis") && log.Pass.Equals("1234"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Denis01/Denis01/Vista/FrmLogin.cs
@@ -0,0 +1,58 @@
+﻿using Denis01.Dominio;
+using Denis01.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Denis01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            Login entrar = new Login();
+            ClsLogin logica = new ClsLogin();
+
+            entrar.User = txtUser.Text;
+            entrar.Pass = txtPass.Text;
+
+            Boolean R = logica.logeo(entrar);
+
+            if (R == true)
+            {
+                MessageBox.Show("Acceso Consedido");
+            }
+            else
+            {
+                MessageBox.Show("Acceso Restringido ");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
