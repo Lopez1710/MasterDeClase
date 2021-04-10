04dc8fad2d810779715a6280d7479b0652416f9b
denis lopez <69539735+Lopez1710@users.noreply.github.com>
2021-04-10 15:41:16 -0600

    matrices

---
+++ Denis05/Denis01/DAO/ClsDJoin.cs
@@ -0,0 +1,48 @@
+﻿using Denis01.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denis01.DAO
+{
+    class ClsDJoin
+    {
+
+        public List<List<object>> JoinUsuarioType()
+        {
+            List<List<object>> Matriz = new List<List<object>>();
+            using (programacionEntities db = new programacionEntities())
+            {
+                var lista = (from Usuario in db.UserList
+                            from Type in db.typeOfUser
+                            where Usuario.IdTypeOUser == Type.IdTypeOfUser
+
+
+                            select new
+                            {
+                                Usuario.id,
+                                Usuario.NombreUsuario,
+                                Type.IdTypeOfUser,
+                                Type.TypeOfUserName
+                            }
+                            ).ToList();
+
+                for(int i=0; i < lista.Count; i++)
+                {
+                    Matriz.Add(new List<object> {
+
+                    lista[i].id,lista[i].NombreUsuario,lista[i].IdTypeOfUser,lista[i].TypeOfUserName
+                    
+                    });
+                }
+
+
+
+
+                return Matriz;
+            }
+        }
+    }
+}
--- Denis05/Denis01/Program.cs
+++ Denis05/Denis01/Program.cs
@@ -17,7 +17,7 @@ namespace Denis01
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmUsuarios());
+            Application.Run(new FrmJoin());
         }
     }
 }
